b4e9c5400aa118c09cd4ed1c8bf281a486ab092d
sim <s5.pentin@yandex.ru>
2024-02-20 20:57:40 +0300

    Start project

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+
+# class Name:
+DATA = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_train\\raw_crowd_train.tsv"
+AUDIO = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_train\\wavs\\"
+
+    # def __init__(self):
+    #     self.DATA =
+    #     self.AUDIO =
+
