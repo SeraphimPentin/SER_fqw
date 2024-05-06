2b7a066125b39c8418edf4c7fbf7973cdd55cead
sim <s5.pentin@yandex.ru>
2024-05-06 16:05:45 +0300

    created a csv file for 5k and 15k records

--- src/constants.py
+++
@@ -1,9 +0,0 @@
-
-DATA_TRAIN = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_train\\raw_crowd_train.tsv"
-AUDIO_TRAIN = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_train\\wavs\\"
-
-DATA_TEST = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_test\\raw_crowd_test.tsv"
-AUDIO_TEST = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_test\\wavs\\"
-
-
-#%%
