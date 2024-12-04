11b3460f150990cb33dbfdb771f1aa2420571031
s.pentin <s.pentin@vk.team>
2024-12-04 17:37:51 +0300

    refactoring

--- src/cnn.py
+++ src/cnn.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -6,215 +6,211 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, classification_report
-import librosa
-import librosa.display
+from sklearn.model_selection import train_test_split
 from keras.src.models import Sequential
 from keras.src.layers import Conv1D, MaxPooling1D, Dropout, Flatten, Dense
 from keras.src.callbacks import ReduceLROnPlateau
-
-#%%
-DATA_TRAIN = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_train\\raw_crowd_train.tsv"
-AUDIO_TRAIN = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_train\\wavs\\"
-
-DATA_TEST = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_test\\raw_crowd_test.tsv"
-AUDIO_TEST = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_test\\wavs\\"
-
-EPOCH = 50
-
-data_train = pd.read_csv(DATA_TRAIN, delimiter='\t')
-data_train.tail()
-#%%
-def create_tsv_file_for_N_records(value: int):
-    # Равное количество записей для каждой эмоции
-    emotions = ['angry', 'neutral', 'sad', 'positive']
-    records_per_emotion = value // len(emotions)
-
-    # Пустой DataFrame для хранения выбранных записей
-    selected_data = pd.DataFrame(columns=data_train.columns)
-
-    # Выбор равного количества записей для каждой эмоции
-    for emo in emotions:
-        emo_data = data_train[(data_train['annotator_emo'] == emo) & (data_train['speaker_emo'] == emo)].head(records_per_emotion)
-        selected_data = pd.concat([selected_data, emo_data])
-
-    # Путь для сохранения нового файла
-    raw_crowd_train = f"C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_{value}.tsv"
-
-    # Сохранение выбранных данных в новом TSV файле
-    selected_data.to_csv(raw_crowd_train, sep='\t', index=False)
-
-#%%
-create_tsv_file_for_N_records(5000)
-
-#% Извлечение признаков
-
-def extract_zcr(data):
-    zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
-    return zcr
-
-def extract_mel_spectrogram(data, sample_rate):
-    mel_spectrogram = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
-    return mel_spectrogram
-
-def extract_mfcc(data, sample_rate):
-    mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
-    return mfcc
-
-def extract_chroma_stft(data, sample_rate):
-    stft = np.abs(librosa.stft(data))
-    chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
-    return chroma_stft
-
-def extract_features(data_path, emotions):
-    X, Y = [], []
-    for path, emotion in zip(data_path, emotions):
-        data, sample_rate = librosa.load(path)
-
-        zcr = extract_zcr(data)
-        mel_spectrogram = extract_mel_spectrogram(data, sample_rate)
-        mfcc = extract_mfcc(data, sample_rate)
-        # chroma_stft = extract_chroma_stft(data, sample_rate)
-
-        features = np.hstack((zcr, mel_spectrogram, mfcc))#, chroma_stft
-        X.append(features)
-        Y.append(emotion)
-
-    return np.array(X), np.array(Y)
-def get_features(path):
-    # duration and offset are used to take care of the no audio in start and the ending of each audio files as seen above.
-    data, sample_rate = librosa.load(path, offset=0.6)
-
-    result = np.array(extract_features(data, sample_rate))
-
-    return result
-
-#%%
-# data_10_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_10.tsv",delimiter='\t')
-# data_100_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_100.tsv", delimiter='\t')
-# data_200_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_200.tsv delimiter='\t')
-# data_400_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_400.tsv", delimiter='\t')
-# data_1000_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_1000.tsv", delimiter='\t')
-# data_2000_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_2000.tsv", delimiter='\t')
-# data_5000_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_5000.tsv", delimiter='\t')
-# data_10_000_records = pd.read_csv("C:\\Users\\s5pen\\DataS`pellProjects\\SER\\data_tcv\\raw_crowd_train_10000.tsv", delimiter='\t')
-# data_15_000_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_15000.tsv",                             delimiter='\t')
-data_25_000_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_25000.tsv", delimiter='\t')
-# data_50_000_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_50000.tsv", delimiter='\t')
-# data_100_000_records = pd.read_csv("C:\\Users\\s5pen\\DataSpellProjects\\SER\\data_tcv\\raw_crowd_train_100000.tsv", delimiter='\t')
-#%%
-audio_path = AUDIO_TRAIN + data_25_000_records.hash_id + ".wav"
-emotions = data_25_000_records.annotator_emo
-
-X, Y = extract_features(audio_path, emotions)
-
-#%%
-len(X), len(Y), audio_path.shape
-
-#%%
-Features = pd.DataFrame(X)
-Features['labels'] = Y
-# Сохранение в CSV
-Features.to_csv('C:\\Users\\s5pen\\DataSpellProjects\\SER\\features\\features_25000.csv', index=False)
-
-#%% Подготовка данных
-X = Features.iloc[: ,:-1].values
-Y = Features['labels'].values
-#%%
-encoder = OneHotEncoder()
-Y = encoder.fit_transform(np.array(Y).reshape(-1,1)).toarray()
-#%%
-from sklearn.model_selection import train_test_split
-
-# splitting data
-x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=0, shuffle=True)
-x_train.shape, y_train.shape, x_test.shape, y_test.shape, x_train
-
-#%%
-# scaling our data with sklearn's Standard scaler
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
-x_train.shape, y_train.shape, x_test.shape, y_test.shape, x_train
-
-#%% PCA
 from sklearn.decomposition import PCA
+import os
 
+# #%%
+# Windows
+# DATA_TRAIN = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_train\\raw_crowd_train.tsv"
+# AUDIO_TRAIN = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_train\\wavs\\"
+
+# MacOS
+DATA_TRAIN = "/Users/s.pentin/Yandex.Disk.localized/Учеба/ДИПЛОМ/DataSet/crowd/crowd_train/raw_crowd_train.tsv"
+AUDIO_TRAIN = "/Users/s.pentin/Yandex.Disk.localized/Учеба/ДИПЛОМ/DataSet/crowd/crowd_train/wavs/"
+
+# Windows
+# DATA_TEST = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_test\\raw_crowd_test.tsv"
+# AUDIO_TEST = "C:\\Users\\s5pen\\YandexDisk\\ВКР\\crowd_test\\wavs\\"
+
+EPOCH = 100
+
+
+def distribution_data(file):
+    X = file.iloc[:, :-1].values
+    Y = file['labels'].values
+    return X, Y
+
+
+def one_hot_encoder(Y):
+    encoder = OneHotEncoder()  # As this is a multiclass classification problem onehotencoding our Y.
+    Y = encoder.fit_transform(np.array(Y).reshape(-1, 1)).toarray()
+    return encoder, Y
+
+
+# %%
+# splitting data
+def splitting_data(X, Y, test=0.2):
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test, random_state=0, shuffle=True)
+    return x_train, x_test, y_train, y_test
+
+
+# %%
+# scaling our data with sklearn's Standard scaler
+def standard_data(x_train, x_test):
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+    return x_train, x_test
+
+
+# %%
 # Применение PCA
-pca = PCA(n_components=0.9)  # Выбор числа компонент, сохраняющего 90% дисперсии
-x_train_pca = pca.fit_transform(x_train)
-x_test_pca = pca.transform(x_test)
+def pca_for_n_component(x_train, x_test, component=0.9):
+    pca = PCA(n_components=component)  # Выбор числа компонент, сохраняющего 90% дисперсии n_components=0.9
+    x_train_pca = pca.fit_transform(x_train)
+    x_test_pca = pca.transform(x_test)
+    return x_train_pca, x_test_pca
 
-x_train_pca.shape, x_train_pca
-#%%
-model=Sequential()
-model.add(Conv1D(256, kernel_size=5, strides=1, padding='same', activation='relu', input_shape=(x_train.shape[1], 1)))
-model.add(MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
 
-model.add(Conv1D(256, kernel_size=5, strides=1, padding='same', activation='relu'))
-model.add(MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
+# %%
+def create_model(x_train):
+    model = Sequential()
+    model.add(Conv1D(256, kernel_size=5, strides=1, padding='same', activation='relu',
+                     input_shape=(x_train.shape[1], 1)))  # 256 -> 128
+    model.add(MaxPooling1D(pool_size=5, strides=2, padding='same'))
 
-model.add(Conv1D(128, kernel_size=5, strides=1, padding='same', activation='relu'))
-model.add(MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
-model.add(Dropout(0.2))
+    # model.add(Conv1D(256, kernel_size=5, strides=1, padding='same', activation='relu'))  # add 256
+    # model.add(MaxPooling1D(pool_size=5, strides=2, padding='same'))
+    #
+    # model.add(Conv1D(512, kernel_size=5, strides=1, padding='same', activation='relu'))  # add 512
+    # model.add(MaxPooling1D(pool_size=5, strides=2, padding='same'))
 
-model.add(Conv1D(64, kernel_size=5, strides=1, padding='same', activation='relu'))
-model.add(MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
+    model.add(Conv1D(256, kernel_size=5, strides=1, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=5, strides=2, padding='same'))
 
-model.add(Flatten())
-model.add(Dense(units=32, activation='relu'))
-model.add(Dropout(0.3))
+    model.add(Conv1D(128, kernel_size=5, strides=1, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=5, strides=2, padding='same'))
+    model.add(Dropout(0.2))
 
-model.add(Dense(units=4, activation='softmax')) # unites - количество эмоций для обучения
-model.compile(optimizer = 'adam' , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
+    model.add(Conv1D(64, kernel_size=5, strides=1, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=5, strides=2, padding='same'))
 
-model.summary()
-#%%
-rlrp = ReduceLROnPlateau(monitor='loss', factor=0.4, verbose=0, patience=2, min_lr=0.0000001)
-history=model.fit(x_train, y_train, batch_size=64, epochs=EPOCH, validation_data=(x_test, y_test), callbacks=[rlrp])
-#%%
-print("Accuracy of our model on test data : " , model.evaluate(x_test,y_test)[1]*100 , "%")
+    model.add(Flatten())
+    model.add(Dense(units=32, activation='relu'))
+    model.add(Dropout(0.3))
 
-epochs = [i for i in range(EPOCH)]
-fig , ax = plt.subplots(1,2)
-train_acc = history.history['accuracy']
-train_loss = history.history['loss']
-test_acc = history.history['val_accuracy']
-test_loss = history.history['val_loss']
+    model.add(Dense(units=4, activation='softmax'))  # unites - количество эмоций для обучения
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-fig.set_size_inches(20,6)
-ax[0].plot(epochs , train_loss , label = 'Training Loss')
-ax[0].plot(epochs , test_loss , label = 'Testing Loss')
-ax[0].set_title('Training & Testing Loss')
-ax[0].legend()
-ax[0].set_xlabel("Epochs")
+    model.summary()
+    return model
 
-ax[1].plot(epochs , train_acc , label = 'Training Accuracy')
-ax[1].plot(epochs , test_acc , label = 'Testing Accuracy')
-ax[1].set_title('Training & Testing Accuracy')
-ax[1].legend()
-ax[1].set_xlabel("Epochs")
-plt.show()
-#%%
+
+# %%
+def fit_model(model: Sequential, x_train, y_train, x_test, y_test, epoch):
+    rlrp = ReduceLROnPlateau(monitor='loss', factor=0.4, verbose=0, patience=2, min_lr=0.0000001)
+    history = model.fit(x_train, y_train, batch_size=64, epochs=epoch, validation_data=(x_test, y_test),
+                        callbacks=[rlrp])
+    return history
+
+
+def print_accuracy(save_dir, model: Sequential, history, x_test, y_test, value):
+    # Оценка точности модели на тестовых данных
+    accuracy = model.evaluate(x_test, y_test)[1] * 100
+    print("Accuracy of our model on test data : ", accuracy, "%")
+
+    epochs = [i for i in range(len(history.history['accuracy']))]
+    train_acc = history.history['accuracy']
+    train_loss = history.history['loss']
+    test_acc = history.history['val_accuracy']
+    test_loss = history.history['val_loss']
+
+    # Создание единого графика с двумя подграфиками
+    fig, ax = plt.subplots(2, 1, figsize=(20, 12))
+
+    # График потерь
+    ax[0].plot(epochs, train_loss, label='Training Loss')
+    ax[0].plot(epochs, test_loss, label='Testing Loss')
+    ax[0].set_title('Training & Testing Loss')
+    ax[0].legend()
+    ax[0].set_xlabel("Epochs")
+    ax[0].set_ylabel("Loss")
+
+    # График точности
+    ax[1].plot(epochs, train_acc, label='Training Accuracy')
+    ax[1].plot(epochs, test_acc, label='Testing Accuracy')
+    ax[1].set_title('Training & Testing Accuracy')
+    ax[1].legend()
+    ax[1].set_xlabel("Epochs")
+    ax[1].set_ylabel("Accuracy")
+
+    # Сохранение графика
+    combined_plot_path = os.path.join(save_dir, f'training_testing_loss_accuracy_{value}_PCA.png')
+    plt.tight_layout()  # Уплотнение подграфиков
+    plt.savefig(combined_plot_path)
+    plt.close(fig)  # Закрытие графика после сохранения
+
+    # Отображение графика
+    plt.show()
+
+
+# %%
 # predicting on test data.
-pred_test = model.predict(x_test)
-y_pred = encoder.inverse_transform(pred_test)
+def print_confusion_matrix(save_dir, y_pred, y_test, encoder, value):
+    y_test = encoder.inverse_transform(y_test)
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(12, 10))
+    cm = pd.DataFrame(cm, index=[i for i in encoder.categories_], columns=[i for i in encoder.categories_])
+    sns.heatmap(cm, linecolor='white', cmap='Blues', linewidth=1, annot=True, fmt='')
+    plt.title('Confusion Matrix', size=20)
+    plt.xlabel('Predicted Labels', size=14)
+    plt.ylabel('Actual Labels', size=14)
 
-y_test = encoder.inverse_transform(y_test)
-cm = confusion_matrix(y_test, y_pred)
-plt.figure(figsize = (12, 10))
-cm = pd.DataFrame(cm , index = [i for i in encoder.categories_] , columns = [i for i in encoder.categories_])
-sns.heatmap(cm, linecolor='white', cmap='Blues', linewidth=1, annot=True, fmt='')
-plt.title('Confusion Matrix', size=20)
-plt.xlabel('Predicted Labels', size=14)
-plt.ylabel('Actual Labels', size=14)
-plt.show()
-df = pd.DataFrame(columns=['Predicted Labels', 'Actual Labels'])
-df['Predicted Labels'] = y_pred.flatten()
-df['Actual Labels'] = y_test.flatten()
-#%%
-df = pd.DataFrame(columns=['Predicted Labels', 'Actual Labels'])
-df['Predicted Labels'] = y_pred.flatten()
-df['Actual Labels'] = y_test.flatten()
+    df = pd.DataFrame(columns=['Predicted Labels', 'Actual Labels'])
+    df['Predicted Labels'] = y_pred.flatten()
+    df['Actual Labels'] = y_test.flatten()
+    # plt.show()
 
-df.head(10)
-print(classification_report(y_test, y_pred))
+    # Сохраняем график
+    plt.savefig(os.path.join(save_dir, f'confusion_matrix_{value}_PCA.png'))
+    plt.close()  # Закрываем график, чтобы освободить память
+
+    # result in console
+    print(classification_report(y_test, y_pred))
+
+
+def main():
+    file_100k = pd.read_csv('/Users/s.pentin/PycharmProjects/SER_fqw/features/features_100000.csv')
+    # print(file_100k.shape)
+    # file = pd.read_csv('/Users/s.pentin/PycharmProjects/SER_fqw/features/features_2000.csv')
+    layer = 6
+    value_pca = "without"  # в процентах компоненты ПСА
+    epoch = 100
+
+    X, Y = distribution_data(file_100k)
+    encoder, Y = one_hot_encoder(Y)
+
+    x_train, x_test, y_train, y_test = splitting_data(X, Y)
+
+    print(x_train.shape)
+    print(x_test.shape)
+
+    x_train, x_test = standard_data(x_train, x_test)
+
+    # x_train_pca, x_test_pca = pca_for_n_component(x_train, x_test, component=value_pca / 100)
+
+    # print(x_train.shape)
+    # print(x_test_pca.shape)
+
+    model = create_model(x_train)
+
+    history = fit_model(model, x_train, y_train, x_test, y_test, epoch)
+
+    pred_test = model.predict(x_test)
+    y_pred = encoder.inverse_transform(pred_test)
+
+    # Директория для сохранения графиков
+    save_dir = f'/Users/s.pentin/PycharmProjects/SER_fqw/resources/pictures/{layer}_layers_100k'
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    print_accuracy(save_dir, model, history, x_test, y_test, value_pca)
+    print_confusion_matrix(save_dir, y_pred, y_test, encoder, value_pca)
+
+
+if __name__ == '__main__':
+    main()
